88509d28288f851852b7b319475e198312ad9a04
tong2696 <843394803@qq.com>
2018-03-23 10:32:59 +0800

    init2

---
+++ project/my-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">北京</h1>
+        </header>
+        <p className="App-intro">
+          欢迎你！！
+        </p>
+      </div>
+    );
+  }
+}
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
